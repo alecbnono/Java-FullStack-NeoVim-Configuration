9edf0da0af7c45836abb7f66d0576c99ebb334ac
Alec Nono <alecbnono@gmail.com>
2025-04-19 02:03:39 +0800

    Add settings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,10 +1,13 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	config = function()
-		vim.keymap.set("n", "\\", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle [E]xplorer" })
-		require("nvim-tree").setup({
-			hijack_netrw = true,
-			auto_reload_on_write = true,
-		})
-	end,
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			vim.keymap.set("n", "\\", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle [E]xplorer" })
+			require("nvim-tree").setup({
+				hijack_netrw = true,
+				auto_reload_on_write = true,
+			})
+		end,
+	},
 }
